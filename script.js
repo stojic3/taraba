f5000e5a616472b5540078a1e3630032d3a3cb83
unknown <olgica12stojic@gmail.com>
2022-09-07 22:43:44 +0200

    SCSS BASE & CAROUSEL

---
+++ script.js
@@ -0,0 +1,29 @@
+import words from "./words.json" assert { type: "json" };
+
+const cardContainers = document.querySelectorAll(".carousel__card_container");
+
+const renderWord = function (word) {
+  return `
+            <div class="card">
+              <div class="card__img_container">
+                <img
+                  class="card__img"
+                  src=${word.img}
+                  alt=""
+                />
+              </div>
+              <div class="card__words">
+                <h3 class="serbian__word">#${word.serbian}</h3>
+                <h4 class="english__word">${word.english}</h4>
+              </div>`;
+};
+
+const displayWords = function (arr) {
+  arr.forEach((word) => {
+    cardContainers.forEach((container) =>
+      container.insertAdjacentHTML("beforeend", renderWord(word))
+    );
+  });
+};
+
+displayWords(words);
